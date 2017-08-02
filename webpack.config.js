b6da253b6ef86c8bc4a7f69ed31b99837e313cd6
sunnySZ <lkp528@163.com>
2017-08-03 00:56:30 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: './',
+        publicPath: '/dist/',
         filename: 'build.js'
     },
     module: {

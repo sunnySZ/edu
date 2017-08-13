b2258e69230c76459f30d227b2ed9bb081225fb9
sunnySZ <lkp528@163.com>
2017-08-13 11:50:22 +0800

    ad

--- src/components/order.vue
+++ src/components/order.vue
@@ -1,24 +1,23 @@
 <template>
     <div class="order_page">
 
-            <h2>凯德西城丨撒浪嘿哟，玩味CP大比拼</h2>
-            <div class="order_item">
+        <h2>凯德西城丨撒浪嘿哟，玩味CP大比拼</h2>
+        <div class="order_item">
             <mt-cell title="特别说明：" value="fdfdfdf"></mt-cell>
             <mt-cell title="兑换说明 ：" value="dfddf" is-link to="tel:8888888"></mt-cell>
             <mt-cell title="退订说明：" @click.native="openAlert('xxxx')" value="xxxxxx"
                      is-link></mt-cell>
-            </div>
+        </div>
         <div class="order_item">
-        <mt-cell title="选择时间"></mt-cell>
-        <mt-cell title="2017-08-26 10:00---2017-08-30 10:00" class="itembg"></mt-cell>
-        <mt-cell title="选择出行日期" is-link></mt-cell>
+            <mt-cell title="选择时间"></mt-cell>
+            <mt-cell title="2017-08-26 10:00---2017-08-30 10:00" class="itembg"></mt-cell>
+            <mt-cell title="选择出行日期" is-link></mt-cell>
         </div>
 
         <div class="order_item">
             <mt-cell title="选择地点"></mt-cell>
-            <mt-cell title="西沙群岛"  value="中国" class="itembg"></mt-cell>
+            <mt-cell title="西沙群岛" value="中国" class="itembg"></mt-cell>
         </div>
-
 
 
         <ul class="footer">
@@ -55,11 +54,24 @@
 </script>
 <style lang="less">
     .order_page {
-     h2{ font-size: 1.6rem; padding: 1rem;}
-        .order_item{ margin-bottom: 1rem;}
-        .itembg{ background-color: orangered; color: white;}
-
-
+        h2 {
+            font-size: 1.6rem;
+            padding: 1rem;
+            background-color:white;
+        }
+        .order_item {
+            margin-bottom: 1rem;
+        }
+        .itembg .mint-cell-wrapper {
+            background-color: orangered;
+            color: white;
+            box-sizing: border-box;
+            border-radius: 0.5rem;
+            margin: 0 1rem 1rem;
+            width: auto;
+            background-size:120% 0;
+        }
+        .itembg .mint-cell-value{ color: white}
 
         .footer {
             display: flex;
@@ -72,15 +84,17 @@
         .footer li {
             display: block;
             width: 60%;
-            height: 3.6rem;
-            line-height: 3.6rem;
+            height:4rem;
+            line-height: 4rem;
             text-align: center;
             font-size: 1.6rem;
             border-top: 1px solid #ccc;
             box-sizing: border-box;
             color: #6d6d6d;
         }
-        .footer li span{ color:#fa6e51;}
+        .footer li span {
+            color: #fa6e51;
+        }
         .footer li:last-child {
             width: 40%;
             background-color: #fa6e51;
@@ -88,7 +102,6 @@
             border-top-color: #fa6e51;
 
         }
-
 
     }
 </style>

035b423a2343e3a686f30cf4e8718e03a622b396
sunnySZ <lkp528@163.com>
2017-08-15 02:06:14 +0800

    add

--- src/store/states.js
+++ src/store/states.js
@@ -2,7 +2,7 @@ let states={
     count:1,
     show:'',
     list:['apple','banana','pear','orange'],
-    userInfo:localStorage.getItem("userInfo")?JSON.parse(localStorage.getItem("userInfo")):{}
+    userInfo:localStorage.getItem("user_Info")?JSON.parse(localStorage.getItem("user_Info")):{}
    /*  userInfo: { //保存用户信息
         nick: null,
         ulevel: null,

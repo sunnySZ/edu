e6e3677e9103a9a2b14aeb6a5ef9977208fa3bfe
sunnySZ <lkp528@163.com>
2017-09-09 01:06:03 +0800

    add

---
+++ src/components/age.vue
@@ -0,0 +1,172 @@
+<template>
+    <div class="age_page">
+        <!--  <p v-show="listLoading" class="loading">
+              <mt-spinner color="#26a2ff" type="fading-circle"></mt-spinner>
+          </p>-->
+        <mt-navbar class="page-part" v-model="selected" fixed>
+            <mt-tab-item v-for="(item,index) in tagData" :id="index+1">{{item.NAME}}</mt-tab-item>
+        </mt-navbar>
+        <ul class="hot_list">
+            <mt-loadmore :top-method="loadTop" @top-status-change="handleTopChange" ref="loadmore"
+                         :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" topLoadingText="加载中...">
+                <li v-for="(item,index) in tabData">
+                    <router-link :to="{ name: 'detail',params: { id: item.ID }}">
+                        <img :src="item.S_PIC"/>
+                        <aside class="aside">
+                            <p>{{item.NAME}}</p>
+                            <span>返利18元</span>
+                            <span>分享有礼</span>
+                            <span>点评有礼</span>
+                        </aside>
+                        <span class="price">￥<mark>{{item.PRICE}}</mark>起</span>
+                        <span class="num">已售94份</span>
+                    </router-link>
+                </li>
+            </mt-loadmore>
+        </ul>
+
+
+        <Foot></Foot>
+    </div>
+</template>
+<script>
+    import Foot from './footer.vue'
+    export default{
+        data(){
+            return {
+                listLoading: false,
+                allLoaded: true,//默认下拉数据加载完毕，不调用loadBottom方法
+                selected: 1,
+                nodata:false,
+                params: {
+                    curPage: 1,
+                    pageSize: 5
+                },
+                tagData: [],
+                tabData: [],
+                agetags: "0,2"
+            }
+        },
+        watch: {
+            selected(curVal, oldVal){
+                switch (curVal) {
+                    case 1:
+                        this.agetags="0,0.5";
+                        break;
+                    case 2:
+                        this.agetags="0.5,1";
+                        break;
+                    case 3:
+                        this.agetags="1,2";
+                        break;
+                    case 4:
+                        this.agetags="2,3";
+                        break;
+                    case 5:
+                        this.agetags="3,6";
+                        break;
+                    case 6:
+                        this.agetags="6,12";
+                        break;
+                }
+                //初始化数据
+                this.tabData = [];
+                this.nodata = false;
+                this.$indicator.open();
+               this.getData(true)
+            }
+        },
+        created(){
+            this.getAgelist();
+        },
+        methods: {
+            getAgelist(){
+                this.$indicator.open();
+              //  this.listLoading=true; //控制
+                let httpArr = [
+                    this.$http.get('yjt/shopgoods/agetaglist'), //年龄标签
+                    this.$http.get('yjt/shopgoods/pagelist/1-5?agetag=0,2') //年龄商品搜索
+                ];
+                //  var url = 'yjt/shopgoods/agetaglist';
+                this.$http.all(httpArr).then(this.$http.spread((data1, data2) => {
+                    this.$indicator.close();
+                    this.tagData = data1.data;
+                    this.tabData = data2.data.list;
+              //      this.listLoading=false;
+                }));
+            },
+            getData(isRefresh){
+                //isRefresh,bool==true是下拉刷新，false表示是上拉加载更多
+                if (this.listLoading) return;
+                this.listLoading = true;
+                if (isRefresh) {
+                    this.params.curPage = 1
+                } else {
+                    this.params.curPage += 1
+                }
+                //   附近活动列表 {当前页}-{每页显示条数}   dis单位千米 0.5表示500米内的活动
+                // yjt/shopgoods/nearbypagelist/1-5?lat=22.67165&lng=114.014654&dis=0.5
+                let url = 'yjt/shopgoods/pagelist/' + this.params.curPage + '-' + this.params.pageSize + '?agetag=' + this.agetags;
+                this.$http.get(url).then((res) => {
+                    this.$indicator.close()
+                    let lists = res.data.list;
+                    this.listLoading = false;
+                    if (this.params.curPage >= res.data.totalPage) {
+                        this.allLoaded = true;
+                        if (this.params.curPage != 1) {
+                            this.$toast('没有更多了...')
+                        }
+                    } else {
+                        this.allLoaded = false;
+                    }
+                    if (isRefresh) {
+                        this.tabData = lists
+                        //  this.$indicator.close();//隐藏loading
+                        this.$refs.loadmore.onTopLoaded();//关闭下拉loading动画
+                    } else {
+                        this.tabData = this.tabData.concat(lists)
+                        this.$refs.loadmore.onBottomLoaded();//关闭上拉loading动画
+                    }
+                }).catch((err) => {
+                    //上下拉loading动画关闭
+                    if (isRefresh) {
+                        this.$indicator.close();//隐藏loading
+                        this.$refs.loadmore.onTopLoaded();
+                    } else {
+                        this.$refs.loadmore.onBottomLoaded();
+                    }
+                    console.log(err)
+                });
+            },
+            loadTop(){
+                //下拉刷新数据
+                this.getData(true)
+            },
+            handleTopChange(status){
+                this.topStatus = status;
+            },
+            loadBottom(){
+
+                //上拉加载更多
+                if (this.listLoading) {
+                    this.$refs.loadmore.onBottomLoaded();
+                } else {
+                    this.allLoaded = true;
+                    this.getData(false)
+                }
+            }
+        },
+        components: {
+            Foot
+        }
+    }
+</script>
+<style lang="less">
+    .age_page {
+
+        padding-top: 60px;
+        padding-bottom: 56px;
+
+
+    }
+</style>
--- src/routerConfig.js
+++ src/routerConfig.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter)
 import Activity from './components/activity.vue'
 import Select from './components/select.vue'
 import My from './components/my.vue'
-import Ticket from './components/ticket.vue'
+//import Ticket from './components/ticket.vue'
 import Search from './components/search.vue'
 import Detail from './components/detail.vue'
 import Discover from './components/discover.vue'
@@ -20,6 +20,7 @@ import CouponSelect from './components/coupon_select.vue'
 import Comments from './components/comments.vue'
 import MyActivity from './components/myActivity.vue'
 import MyActivityInfo from './components/myActivityInfo.vue'
+import Age from './components/age.vue'
 /*const my_routes = [
  {name: 'home', path: '/', component: List},
  {name: 'infinite', path: '/infinite', component: Infinite},
@@ -31,41 +32,42 @@ import MyActivityInfo from './components/myActivityInfo.vue'
  })*/
 
 const router = new VueRouter({
-   scrollBehavior: () => ({y: 0}),
+    scrollBehavior: () => ({y: 0}),
     mode: 'hash',
     routes: [
-        {path: '*', redirect: '/select/',meta:{title:'精选'}},//默认显示
-        {name: 'select', path: '/select/', component: Select,meta:{title:'精选'}},
-        {name: 'activity', path: '/activity', component: Activity,meta:{title:'活动'}},
-        {name: 'ticket', path: '/ticket', component: Ticket,meta:{title:'购票'}},
-        {name: 'my', path: '/my', component: My,meta:{title:'个人中心',auth:true}},
-        {name: 'search', path: '/search', component: Search,meta:{title:'搜索'}},
-        {name: 'detail', path: '/detail/:id', component: Detail,meta:{title:'活动详情'}},
-        {name: 'discover', path: '/discover/:type', component: Discover,meta:{title:'发现'}},
-        {name: 'order', path: '/order', component: Order,meta:{title:'订单确认',auth:true}},
-        {name: 'orderPay', path: '/order/orderPay', component: OrderPay,meta:{title:'收银台',auth:true}},
-        {name: 'orderList', path: '/orderList', component: OrderList,meta:{title:'订单列表',auth:true}},
-        {name: 'collect', path: '/collect', component: Collect,meta:{title:'我的收藏',auth:true}},
-        {name: 'coupon', path: '/coupon', component: Coupon,meta:{title:'优惠券',auth:true}},
-        {name: 'scores', path: '/scores', component: Scores,meta:{title:'积分',auth:true}},
-        {name: 'couponSelect', path: '/coupon_select', component: CouponSelect,meta:{title:'选择优惠券',auth:true}},
-        {name: 'comments', path: '/comments', component: Comments,meta:{title:'评价',auth:true}},
-        {name: 'myActivity', path: '/myActivity', component: MyActivity,meta:{title:'我的活动',auth:true}},
-        {name: 'myActivityInfo', path: '/myActivityInfo', component: MyActivityInfo,meta:{title:'我的活动',auth:true}}
+        {path: '*', redirect: '/select/', meta: {title: '精选'}},//默认显示
+        {name: 'select', path: '/select/', component: Select, meta: {title: '精选'}},
+        {name: 'activity', path: '/activity', component: Activity, meta: {title: '活动'}},
+        // {name: 'ticket', path: '/ticket', component: Ticket,meta:{title:'购票'}},
+        {name: 'my', path: '/my', component: My, meta: {title: '个人中心', auth: true}},
+        {name: 'search', path: '/search', component: Search, meta: {title: '搜索'}},
+        {name: 'detail', path: '/detail/:id', component: Detail, meta: {title: '活动详情'}},
+        {name: 'discover', path: '/discover/:type', component: Discover, meta: {title: '发现'}},
+        {name: 'order', path: '/order', component: Order, meta: {title: '订单确认', auth: true}},
+        {name: 'orderPay', path: '/order/orderPay', component: OrderPay, meta: {title: '收银台', auth: true}},
+        {name: 'orderList', path: '/orderList', component: OrderList, meta: {title: '订单列表', auth: true}},
+        {name: 'collect', path: '/collect', component: Collect, meta: {title: '我的收藏', auth: true}},
+        {name: 'coupon', path: '/coupon', component: Coupon, meta: {title: '优惠券', auth: true}},
+        {name: 'scores', path: '/scores', component: Scores, meta: {title: '积分', auth: true}},
+        {name: 'couponSelect', path: '/coupon_select', component: CouponSelect, meta: {title: '选择优惠券', auth: true}},
+        {name: 'comments', path: '/comments', component: Comments, meta: {title: '评价', auth: true}},
+        {name: 'myActivity', path: '/myActivity', component: MyActivity, meta: {title: '我的活动', auth: true}},
+        {name: 'myActivityInfo', path: '/myActivityInfo', component: MyActivityInfo, meta: {title: '我的活动', auth: true}},
+        {name: 'age', path: '/age', component: Age, meta: {title: '年龄百宝箱'}}
     ]
 })
 
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to, from, next) => {
     /*if(to.meta.auth&&to.name=='order'){//判断该路由是否需要登录权限
-       if(store.state.user_id){//通过vuex state 获取当前uerInfo是否存在
-          next()
-       }else{
-            var url1='/route.html?code=1';  //授权认证后跳转到order界面
-            window.location.href='index.jsp?url='+encodeURIComponent(url1)
-       }
-    }*/
-    if(to.meta&&to.meta.title){
-        document.title=to.meta.title
+     if(store.state.user_id){//通过vuex state 获取当前uerInfo是否存在
+     next()
+     }else{
+     var url1='/route.html?code=1';  //授权认证后跳转到order界面
+     window.location.href='index.jsp?url='+encodeURIComponent(url1)
+     }
+     }*/
+    if (to.meta && to.meta.title) {
+        document.title = to.meta.title
     }
     next()
 })
